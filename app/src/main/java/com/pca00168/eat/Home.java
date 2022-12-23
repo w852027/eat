5d5fd6f5ea89ece4c51e0ff4b8fdba3827dcc098
korean_fish <@>
2022-12-23 16:06:39 +0800

    歡迎頁面改變數名+新建Home畫面

---
+++ app/src/main/java/com/pca00168/eat/Home.java
@@ -0,0 +1,36 @@
+package com.pca00168.eat;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.pca00168.eat.databinding.HomeBinding;
+
+public class Home extends AppCompatActivity {
+
+    private HomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = HomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_home);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
--- app/src/main/java/com/pca00168/eat/MainActivity.java
+++ app/src/main/java/com/pca00168/eat/MainActivity.java
@@ -1,14 +1,19 @@
 package com.pca00168.eat;
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+    }
+    public void login_click(View view) {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, Home.class);
+        startActivity(intent);
     }
 }
