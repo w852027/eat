ddcd6609fd4e5a48da5759cb3192c798d3b02782
korean_fish <@>
2023-02-02 18:27:47 +0800

    新增使用臉書登入功能

--- app/src/main/java/com/pca00168/eat/MainActivity.java
+++ app/src/main/java/com/pca00168/eat/MainActivity.java
@@ -3,16 +3,51 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StatusBarUtils.setWindowStatusBarColor(this,R.color.black);
+        //FacebookSdk.sdkInitialize(getApplicationContext());
+        //AppEventsLogger.activateApp(this);
+
     }
+    //https://developers.facebook.com/apps/1612259719219926/fb-login/quickstart/
     public void login_click(View view) {
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this, Home.class);
-        startActivity(intent);
+       String EMAIL = "email";
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_login);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        loginButton.registerCallback(CallbackManager.Factory.create(), new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+            @Override
+            public void onCancel() {
+
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+
+            }
+        });
+
     }
 }
