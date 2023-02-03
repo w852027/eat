91ac44300b55435c16549a6f8240c0e0469d0746
korean_fish <@>
2023-02-03 14:12:48 +0800

    弄掉註冊按鈕

--- app/src/main/java/com/pca00168/eat/MainActivity.java
+++ app/src/main/java/com/pca00168/eat/MainActivity.java
@@ -1,34 +1,22 @@
 package com.pca00168.eat;
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
 import java.util.Arrays;
-
 public class MainActivity extends Activity {
     CallbackManager callbackManager;
     GoogleSignInClient mGoogleSignInClient;
@@ -56,7 +44,6 @@ public class MainActivity extends Activity {
 
         }else
             callbackManager.onActivityResult(requestCode, resultCode, data);
-
     }
     private void fb_login(){
        // FacebookSdk.sdkInitialize(getApplicationContext());
@@ -67,7 +54,7 @@ public class MainActivity extends Activity {
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                logged_in();
             }
             @Override
             public void onCancel() {
@@ -99,8 +86,6 @@ public class MainActivity extends Activity {
         intent.setClass(MainActivity.this, Home.class);
         startActivity(intent);
     }
-
-
     public void google_click(View view){
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 1);
