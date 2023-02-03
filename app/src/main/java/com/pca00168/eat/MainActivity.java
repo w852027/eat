9f3e24a99a36138c9fd7d8f8535a19c665bf2851
korean_fish <@>
2023-02-03 14:07:23 +0800

    弄好google登入

--- app/src/main/java/com/pca00168/eat/MainActivity.java
+++ app/src/main/java/com/pca00168/eat/MainActivity.java
@@ -1,8 +1,15 @@
 package com.pca00168.eat;
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -11,36 +18,56 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 import java.util.Arrays;
 
 public class MainActivity extends Activity {
     CallbackManager callbackManager;
+    GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StatusBarUtils.setWindowStatusBarColor(this,R.color.black);
         fb_login();
+        google_login();
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-       callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account!=null)
+                    logged_in();
+            } catch (ApiException e) {
+                err(e.getMessage()+"登入失敗");
+            }
+
+        }else
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
     }
-    //https://developers.facebook.com/apps/1612259719219926/fb-login/quickstart/
     private void fb_login(){
        // FacebookSdk.sdkInitialize(getApplicationContext());
        // AppEventsLogger.activateApp(this);
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.button_login);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_login);
         loginButton.setReadPermissions(Arrays.asList("email", "public_profile", "user_friends"));
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Home.class);
-                startActivity(intent);
+
             }
             @Override
             public void onCancel() {
@@ -53,6 +80,31 @@ public class MainActivity extends Activity {
         });
     }
 
+    private void  google_login(){
+        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+            logged_in();
+            return;
+        }
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestId()
+                .requestProfile()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+    }
+
+    private void logged_in(){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, Home.class);
+        startActivity(intent);
+    }
+
+
+    public void google_click(View view){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1);
+    }
     private void err(String msg){
         Toast.makeText(this,msg , Toast.LENGTH_LONG).show();
     }
