82580a487c9ad11734da6e2af22f8c7f92ae432b
korean_fish <@>
2023-02-10 16:24:53 +0800

    每週報告─消耗

--- app/src/main/java/com/pca00168/eat/weekly_report.java
+++ app/src/main/java/com/pca00168/eat/weekly_report.java
@@ -22,9 +22,10 @@ public class weekly_report extends AppCompatActivity {
                 public_func.timestamp_get( public_func.timestamp_now(),true),
                 public_func.timestamp_get( public_func.timestamp_now(),false)
         ));
-        load_data();
+        load_input_data();
+        load_output_data();
     }
-    private void load_data(){
+    private void load_input_data(){
         LinearLayout input_kcal_table = findViewById(R.id.input_kcal_table);
         for (short type=0;type<=3;type++) {
             kcal_foods foods=User.load_kcal_input(this,type,public_func.timestamp_a_week_ago(),public_func.timestamp_now());
@@ -35,14 +36,31 @@ public class weekly_report extends AppCompatActivity {
             ((TextView)cell.findViewById(R.id.count_value)).setText(String.valueOf(foods.size()));
             ((TextView)cell.findViewById(R.id.kcal_value)).setText(String.valueOf(foods.total_kcal()));
             cell.setOnClickListener(new View.OnClickListener() {
-                
                 public void onClick(View v) {
-
 
                 }
             });
             input_kcal_table.addView(cell);
         }
+    }
+    private void load_output_data(){
+        LinearLayout output_kcal_table = findViewById(R.id.output_kcal_table);
+        for (short type=0;type<=kcal_sports.sport_list().size();type++) {
+            kcal_sports sports=User.load_kcal_output(this,type,public_func.timestamp_a_week_ago(),public_func.timestamp_now());
+            if(sports.size()==0)continue;
+            LayoutInflater layoutInflater = LayoutInflater.from(this);
+            View cell = layoutInflater.inflate(R.layout.weekly_report_table_cell, null);
+            ((TextView)cell.findViewById(R.id.count)).setText("次");
+            ((TextView)cell.findViewById(R.id.type)).setText(sports.get(0).name);
+            ((ImageView)cell.findViewById(R.id.type_icon)).setImageDrawable(getResources().getDrawable(sports.get(0).icon_resource_id));
+            ((TextView)cell.findViewById(R.id.count_value)).setText(String.valueOf(sports.size()));
+            ((TextView)cell.findViewById(R.id.kcal_value)).setText(String.valueOf(sports.total_kcal()));
+            cell.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 
+                }
+            });
+            output_kcal_table.addView(cell);
+        }
     }
 }
